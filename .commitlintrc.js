b7b893b3c8e8f01bf9bcb57b52e22e7e43eeba50
Dongha Kang <dkang0602@gmail.com>
2022-04-11 17:56:22 +0900

    :sparkles: feature: upgrade .husky with commitlint and gitmoji

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,23 @@
-module.exports = {extends: ['gitmoji']};
+module.exports = {
+  extends: ['@commitlint/config-conventional', 'gitmoji'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feature',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'chore',
+        'wip',
+      ],
+    ],
+  },
+}
